9d3f5fa78590b776e44de4c736edb3722d2c3f5e
Андрей Никоненко <sonanted@gmail.com>
2022-05-29 12:24:06 +0300

    flake8 fix

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MaxValueValidator, MinValueValidator
-from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from .utils import get_current_year
 
